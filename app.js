251a9665b29f39298d74b7e02313b8356ea73a39
govardhanreddyc79 <govardhanreddyc79@gmail.com>
2021-09-06 14:50:48 +0530

    Done with mark-6

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-var inputName = prompt('Enter Your Name')
+// var inputName = prompt('Enter Your Name')
 
-var welcomeMessage = "This script works " + inputName
+// var welcomeMessage = "This script works " + inputName
 
-alert(welcomeMessage )
+// alert(welcomeMessage )
+
+var btnTranslate = document.querySelector("#btn-translate")
+var userInput = document.querySelector("textarea")
+var output = document.querySelector("#output")
+
+// var urlSample = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var urlSample = "https://api.funtranslations.com/translate/chef.json"
+
+function generateURL(input){
+    return urlSample + "?" + "text="+ input
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("Something went with server! Please try again")
+}
+
+function clickEventHandler(){
+    var inputText = userInput.value;
+
+    fetch(generateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;  
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler)
+    }
+
+btnTranslate.addEventListener("click",clickEventHandler)
